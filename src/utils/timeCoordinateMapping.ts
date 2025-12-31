bb2eaf33c3046c1808c16a005780d6761a9c0ef0
koki.gotou <koki.gotou@xincere.jp>
2025-12-31 16:11:03 +0900

    feature: [GREEN]Add time-coordinate mapping util function

--- src/utils/timeCoordinateMapping.ts
+++ src/utils/timeCoordinateMapping.ts
@@ -1,9 +1,23 @@
 function timeToCoordinate(time: number, min: number, max: number, width: number): number {
-  return 0
+  if (time < min) {
+    throw new RangeError('Time is less than min');
+  }
+  if (time > max) {
+    throw new RangeError('Time is greater than max');
+  }
+  return (width * (time - min)) / (max - min);
 }
 
 function coordinateToTime(x: number, min: number, max: number, width: number): number {
-  return 0;
+  // Clamp x if it is out of range
+  if (x < 0) {
+    return min;
+  }
+  if (x > width) {
+    return max;
+  }
+
+  return min + (x / width) * (max - min);
 }
 
 export { timeToCoordinate, coordinateToTime };
